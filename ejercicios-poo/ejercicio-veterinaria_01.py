ed8b50653ad1dd0fe99e3a9b3ab94c95812fb6d6
catagit26 <catacalderon.cg@gmail.com>
2026-04-15 13:32:02 -0300

    Update ejercicio-veterinaria_01.py

--- ejercicios-poo/ejercicio-veterinaria_01.py
+++ ejercicios-poo/ejercicio-veterinaria_01.py
@@ -1,1 +1,14 @@
+class Animal:
+    def __init__(self, nombre, especie, edad):
+        self.nombre = nombre
+        self.especie = especie
+        self.edad = edad
+    def __str__(self): #metodo que define como su muestra el objeto al usar print()
+        return f"Nombre: {self.nombre} | Especie: {self.especie} | Edad: {self.edad} años"
 
+nombre = input("Ingrese el nombre del animal: ") #permite pedir datos al usuario
+especie = input("Ingrese la especie: ") #input () devuelve string
+edad = int(input("Ingrese la edad: ")) #int (input()) se convierte a entero
+
+a = Animal (nombre, especie, edad) #creamos objeto con los datos ingresados por usuario
+print(a) #cuando imprime objeto se ejecuta el metodo __str__() automaticamente
